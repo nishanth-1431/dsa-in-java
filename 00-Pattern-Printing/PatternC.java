306518a9e2921e69917a5ea2f064feef5106c36e
nishanth-1431 <hinishanth777@gmail.com>
2026-09-14 21:24:44 +0530

    practice: add Right-Angled Star Triangle - Pattern C

--- 00-Pattern-Printing/PatternC.java
+++ 00-Pattern-Printing/PatternC.java
@@ -1,10 +1,25 @@
-/* Pattern C - 
+/* Pattern C - Right-Angled Star Triangle
+
     *
     **
     ***
     ****
     *****
+
  */
 public class PatternC {
-    
+      public static void main(String[] args) {
+
+       int n = 5;
+
+       for(int i = 1 ; i <=n;i++)            //this loop is used to change the rows
+        {      
+            for (int j = 1; j <= i; j++)      
+            {    
+                System.out.print( "* ");  
+            }
+          System.out.println();            
+       }
+
+    }
 }
