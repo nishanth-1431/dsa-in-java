e341887decf5ae3d1da74f310b2d9581dcea7dc8
nishanth-1431 <hinishanth777@gmail.com>
2026-09-15 02:12:42 +0530

    added temp learning files

---
+++ 00-Pattern-Printing/SumOfElements.java
@@ -0,0 +1,15 @@
+public class SumOfElements {
+
+    public static int SumOfElements(int[] arr) {
+        int sum = 0;
+        for(int i = 0 ; i < arr.length ; i++ ){
+            sum += arr[i];
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        System.out.println("the sum of array is :" + SumOfElements(arr));
+    }
+}
