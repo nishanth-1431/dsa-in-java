c3bb51de3e771c967d0c531925dca026356ba1b7
nishanth-1431 <hinishanth777@gmail.com>
2026-09-16 14:47:48 +0530

    practice: add  Right-Aligned Increasing Star Triangle - Pattern K

---
+++ 00-Pattern-Printing/PatternK.java
@@ -0,0 +1,24 @@
+/* Pattern K - Right-Aligned Increasing Star Triangle.
+      *
+     **
+    ***
+   ****
+  *****
+*/
+public class PatternK {
+    public static void main(String[] args) {
+        int n = 5 ;
+        for (int i = 1 ; i <= n ; i++){
+
+            for (int j = n; j > i; j--) {
+                System.out.print("  ");
+            }
+
+            for (int j = 1; j <= i; j++){
+                System.out.print("* ");
+            }
+            
+            System.out.println();
+        }
+    }
+}
