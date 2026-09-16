198e45d166b1d5dbe3d8242330c18c86fd530c9d
nishanth-1431 <hinishanth777@gmail.com>
2026-09-16 14:05:36 +0530

    practice: add Half Diamond Star Pattern - Pattern J

---
+++ 00-Pattern-Printing/PatternJ.java
@@ -0,0 +1,37 @@
+/* Pattern J - Half Diamond Star Pattern
+
+    *
+    **
+    ***
+    ****
+    *****
+    ****
+    ***
+    **
+    *
+
+*/
+public class PatternJ {
+       public static void main(String[] args) {
+
+       int n = 5;
+
+       for(int i = 1 ; i <=n;i++)            
+        {      
+            for (int j = 1; j <= i; j++)      
+            {    
+                System.out.print( "* ");  
+            }
+          System.out.println();            
+       }
+       for(int i = n-1 ; i > 0 ; i-- )            
+        {      
+            for (int j = i ; j > 0 ; j-- )      
+            {    
+                System.out.print( "* ");  
+            }
+          System.out.println();            
+       }
+    }
+    
+}
