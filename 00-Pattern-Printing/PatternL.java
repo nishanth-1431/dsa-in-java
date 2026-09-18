3fd6c1c1494f6b5dfcd3d1e7614b5cb3bacb7fcf
nishanth-1431 <hinishanth777@gmail.com>
2026-09-18 18:12:57 +0530

    practice

--- 00-Pattern-Printing/PatternL.java
+++ 00-Pattern-Printing/PatternL.java
@@ -6,6 +6,19 @@
       *
 */
 public class PatternL {
+    public static void main(String[] args) {
+        int n = 5 ;
+        for (int i = 1 ; i <= n ; i++){
 
-    
+            for (int j = n; j > i; j--) {
+                System.out.print("* ");
+            }
+
+            for (int j = 1; j <= i; j++){
+                System.out.print("  ");
+            }
+            
+            System.out.println();
+        }
+    }
 }
