337abfa01f85d87e49e8be1d9825377aa0d22533
nishanth-1431 <hinishanth777@gmail.com>
2026-09-22 11:17:16 +0530

    add: solved some basic array problems

---
+++ 00-Pattern-Printing/array.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+class array {
+    public static void main(String args[]) {
+        int[] arr = { 1, 2, 9, 5, 6, 7, 8, 0};
+        for(int i = arr.length-1 ; i>=0;i--){
+            System.out.println(arr[i]);
+        }
+    }
+}
