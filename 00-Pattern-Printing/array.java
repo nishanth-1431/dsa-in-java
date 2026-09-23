9476d1ed8ee1285798c2a98f1f5a959c1cdbaf89
nishanth-1431 <hinishanth777@gmail.com>
2026-09-23 12:20:04 +0530

    add: solved some basic array problems

--- 00-Pattern-Printing/array.java
+++ 00-Pattern-Printing/array.java
@@ -1,10 +1,23 @@
-import java.util.Arrays;
+public class array {
 
-class array {
-    public static void main(String args[]) {
-        int[] arr = { 1, 2, 9, 5, 6, 7, 8, 0};
-        for(int i = arr.length-1 ; i>=0;i--){
-            System.out.println(arr[i]);
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 25, 8, 4, 15};
+
+        int largest = numbers[0];
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int i = 1; i < numbers.length; i++) {
+
+            if (numbers[i] > largest) {
+                secondLargest = largest;
+                largest = numbers[i];
+            }
+            else if (numbers[i] > secondLargest && numbers[i] != largest) {
+                secondLargest = numbers[i];
+            }
         }
+
+        System.out.println("Second largest: " + secondLargest);
     }
 }
